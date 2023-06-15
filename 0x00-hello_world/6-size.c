1fd5a22e66529fd1e6f90fe8e43561e78496eec2
Muraijnr <muraimuchoki50@gmail.com>
2023-06-15 12:30:27 -0700

    a repeat on task 4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int main(void)
+int main(void);
 {
-	printf("Size of a char: %zu bites\n", sizeof(char));
-	printf("Size of an int: %zu bites\n", sizeof(int));
-	printf("Size of a long int: %zu bites\n", sizeof(long int));
-	printf("Size of a long long int: %zu bites\n", sizeof(long long int));
-	printf("Size of a float: %zu bites\n", sizeof(float));
+	printf("Size of a char: %zu bite(s)\n", sizeof(char));
+	printf("Size of an int: %zu bite(s)\n", sizeof(int));
+	printf("Size of a long int: %zu bite(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu bite(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu bite(s)\n", sizeof(float));
 
 	return (0);
 }
