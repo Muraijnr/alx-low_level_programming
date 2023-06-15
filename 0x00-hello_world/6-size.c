6b4b55f01c1069ba5201590e544ae6d4637b3f92
Muraijnr <muraimuchoki50@gmail.com>
2023-06-15 12:20:47 -0700

    Printing the sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-	printf("Size of a char: %zu\n", sizeof(char));
-	printf("Size of an int: %zu\n", sizeof(int));
-	printf("Size of a long int: %zu\n", sizeof(long int));
-	printf("Size of a long long int: %zu\n", sizeof(long long int));
-	printf("Size of a float: %zu\n", sizeof(float));
+	printf("Size of a char: %zu bites\n", sizeof(char));
+	printf("Size of an int: %zu bites\n", sizeof(int));
+	printf("Size of a long int: %zu bites\n", sizeof(long int));
+	printf("Size of a long long int: %zu bites\n", sizeof(long long int));
+	printf("Size of a float: %zu bites\n", sizeof(float));
 
 	return (0);
 }
