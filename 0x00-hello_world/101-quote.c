41ab64b0142cde2072d537381dc001aacf39e402
Muraijnr <muraimuchoki50@gmail.com>
2023-06-15 13:37:59 -0700

    Quote correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,58 +1,13 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - prints out a line without using puts or printf
+ * Return: 1
+ */
+int main(void)
 {
-	char str[] = "2015-10-19";
-	putchar('a');
-	putchar('n');
-	putchar('d');
-	putchar(' ');
-	putchar('t');
-	putchar('h');
-	putchar('a');
-	putchar('t');
-	putchar(' ');
-	putchar('p');
-	putchar('i');
-	putchar('e');
-	putchar('c');
-	putchar('e');
-	putchar(' ');
-	putchar('o');
-	putchar('f');
-	putchar(' ');
-	putchar('a');
-	putchar('r');
-	putchar('t');
-	putchar(' ');
-	putchar('i');
-	putchar('s');
-	putchar(' ');
-	putchar('u');
-	putchar('s');
-	putchar('e');
-	putchar('f');
-	putchar('u');
-	putchar('l');
-	putchar('"');
-	putchar(' ');
-	putchar('-');
-	putchar(' ');
-	putchar('D');
-	putchar('o');
-	putchar('r');
-	putchar('a');
-	putchar(' ');
-	putchar('K');
-	putchar('o');
-	putchar('r');
-	putchar('p');
-	putchar('a');
-	putchar('r');
-	putchar(',');
-	putchar(' ');
-	fputs(str, stdout);
-	putchar('\n');
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
+	fputs(str, stdout);
+	fputs("\n", stdout);
 	return (1);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - prints a line using puts
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
