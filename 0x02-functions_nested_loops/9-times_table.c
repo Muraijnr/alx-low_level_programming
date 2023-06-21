990c308a8c7885a4845bb59bfbfc526e6c554c69
Muraijnr <muraimuchoki50@gmail.com>
2023-06-21 10:19:52 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * times_table - prints times table
+ * Return: empty output
+ */
+void times_table(void)
+{
+	int rows = 10;
+	int cols = 10;
+
+	int a, b, result;
+
+	for (a = 0; a < rows; a++)
+	{
+		for (b = 0; b < cols; b++)
+		{
+			result = a * b;
+
+			if (result < 10)
+			{
+				_putchar('0' + result);
+			}
+			else
+			{
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+			}
+			if (b < cols - 1)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\t');
+		_putchar('\n');
+	}
+}
