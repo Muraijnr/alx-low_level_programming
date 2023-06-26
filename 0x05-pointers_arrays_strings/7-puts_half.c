cf789caab3947bdc305a76ea6f3648d0af22ce64
Muraijnr <muraimuchoki50@gmail.com>
2023-06-26 14:42:40 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - prints half a string
+ * @str: string to be printed
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int full, half;
+
+	full = 0;
+
+	while (str[full] != '\0')
+		full++;
+	half = full / 2;
+
+	if (full % 2 == 1)
+		half++;
+	while (half < full)
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+}
