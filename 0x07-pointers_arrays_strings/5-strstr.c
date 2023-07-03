05fa57b6c81400dec41877b420384dd65506c217
Muraijnr <muraimuchoki50@gmail.com>
2023-07-03 16:21:01 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,10 +10,7 @@ char *_strstr(char *haystack, char *needle)
 	int index;
 
 	if (*needle == 0)
-	{
 		return (haystack);
-	}
-
 	while (*haystack)
 	{
 		index = 0;
@@ -22,12 +19,11 @@ char *_strstr(char *haystack, char *needle)
 		{
 			do {
 				if (needle[index + 1] == '\0')
-				{
 					return (haystack);
-				}
 				index++;
 			} while (haystack[index] == needle[index]);
 		}
 		haystack++;
 	}
+	return ('\0');
 }
