23adada0e1042890498788e0d18dcd37597d78cd
Muraijnr <muraimuchoki50@gmail.com>
2023-07-03 12:27:19 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant bite
+ * @s: pointer
+ * @b: constant bite b
+ * @n: number of bites
+ * Return: pointer s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+	b = 0;
+	while (a )
+}
