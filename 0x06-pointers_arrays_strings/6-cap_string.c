3d060568d7e173912d2e9e40272b3aba7c2512a4
Muraijnr <muraimuchoki50@gmail.com>
2023-07-05 01:42:53 +0300

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * cap_string - capitalize first letter of a word in a string
+ * @str: pointer to a string
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+    int cap_next = 1; //flag to determine whether next character should be capitalized
+    char *result = str; //save the starting address of the string
+
+    while (*str)
+    {
+        if ((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z')) /**
+        *this checks if its lowercase or uppercase
+        */
+        {
+            if (cap_next) //checks if next letter should be capitalized
+            {
+                if (*str >= 'a' && *str <= 'z') //checks if lowercase
+                {
+                    *str = *str - 'a' + 'A'; //capitalize lowercase letter
+                }
+                cap_next = 0; //ensures next characters are not capitalized
+            }
+        } 
+        else //executed if character was not alphabetic
+    {
+        cap_next = 1;
+    }
+    str++;
+    }
+    return (result);
+}
