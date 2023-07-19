ceba37505aa326d03ae0639b50141d1e5a669e75
Muraijnr <muraimuchoki50@gmail.com>
2023-07-19 08:00:23 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t a;
 
-	if (array == NULL && action == NULL)
+	if (array == NULL || action == NULL)
 		return;
 	for (a = 0; a < size; a++)
 	{
