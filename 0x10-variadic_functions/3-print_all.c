1226656fcfe750386742eb5c26933107312b8702
Muraijnr <muraimuchoki50@gmail.com>
2023-07-20 17:13:53 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,80 +1,49 @@
 #include "stdio.h"
-#include "variadic_functions.h"
 #include "stdlib.h"
+#include "variadic_functions.h"
 #include "stdarg.h"
 /**
- * print_c - prints a character
- * @args: arguments to print from
- * Return: void
- */
-void print_c(va_list args)
-{
-	printf("%c", (char) va_arg(args, int));
-}
-/**
- * print_int - prints integers
- * @args: arguments to print from
- * Return: void
- */
-void print_int(va_list args)
-{
-	printf("%d", va_arg(args, int));
-}
-/**
- * print_f - prints a float
- * @args: arguments to print from
- * Return: void
- */
-void print_f(va_list args)
-{
-	printf("%f", (float) va_arg(args, int));
-}
-/**
- * print_s - prints a string
- * @args: arguments to print from
- * Return: void
- */
-void print_s(va_list args)
-{
-	char *sp = va_arg(args, char*);
-
-	while (sp != NULL)
-	{
-		printf("%s", sp);
-		return;
-	}
-	printf("(nil)");
-}
-/**
- * print_all - prints various types depending on format
- * @format: gives type of information for args
+ * print_all - prints all arguments depending on the format given
+ * @format: specifier
  * Return: void
  */
 void print_all(const char * const format, ...)
 {
-	const char *ptr;
+	int a = 0;
+	char *sp, *sep = "";
 	va_list args;
 
-	functionkey key[4] = {{print_c, 'c'}, {print_int, 'i'},
-				{print_f, 'f'}, {print_s, 's'}};
-	int keyindex = 0, notfirst = 0;
-
-	ptr = format;
 	va_start(args, format);
-	while (format != NULL && *ptr)
+
+	if (format)
 	{
-		if (key[keyindex].spec == *ptr)
+		while (format[a])
 		{
-			if (notfirst)
-				printf(", ");
-			notfirst = 1;
-			key[keyindex].f(args);
-			ptr++;
-			keyindex = -1;
+			switch (format[a])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(args, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(args, double));
+					break;
+				case 's':
+					sp = va_arg(args, char *);
+
+					if (!sp)
+						sp = "(nil)";
+					printf("%s%s", sep, sp);
+					break;
+				default:
+					a++;
+					continue;
+			}
+			sep = ", ";
+			a++;
 		}
-		keyindex++;
-		ptr += keyindex / 4;
-		keyindex %= 4;
 	}
 	printf("\n");
 	va_end(args);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,21 +1,10 @@
 #ifndef VA_FUNCTIONS_H
 #define VA_FUNCTIONS_H
-#include "stdarg.h"
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-/**
- * struct functionkey - a key for picking function to use
- * @f: function for use
- * @spec: character specifier
- */
-typedef struct functionkey
-{
-	void(*f)(va_list);
-	char spec;
-} functionkey;
 
 #endif
