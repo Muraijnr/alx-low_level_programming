a7fe438bb1fe91bc29ac1135ae06f660e0f90f6b
Muraijnr <muraimuchoki50@gmail.com>
2023-07-27 19:08:16 -0700

    0-error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,5 +19,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		a++;
 	}
-	return (s);
+	return (a);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
-size_t list_len(const list_t *h);
-int _putchar(char c);
-size_t print_list(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+#ifndef LISTS_H
+#define LISTS_H
 
+#include <stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -21,5 +16,12 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+
+size_t list_len(const list_t *h);
+int _putchar(char c);
+size_t print_list(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
