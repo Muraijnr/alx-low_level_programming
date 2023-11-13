faaf2ac14ccf49c1f9b908a716ece9285f5a1561
Muraijnr <muraimuchoki50@gmail.com>
2023-11-13 06:05:20 -0800

    After deletion

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNC_LIKE_H
-#define FUNC_LIKE_H
-
-#define ABS(X) ((X) < 0 ? -(X) : (X))
-
-typedef int macrofunc;
-
-#endif
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,6 +0,0 @@
-#ifndef SUM_4_H
-#define SUM_4_H
-
-#define SUM(X, Y) ((X) + (Y))
-
-#endif
